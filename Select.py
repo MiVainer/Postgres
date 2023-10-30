956aab88776619b01f34aa5e9c14f8b27f2f8d87
Mihail Vainer <mh93@yandex.ru>
2023-10-30 18:09:44 +0700

    compose2

---
+++ Select.py
@@ -0,0 +1,39 @@
+# -------------------------#
+# ---Program by MiVainer---#
+import psycopg2
+
+outputfile = f"/home/mihail/Postgres/{input('Введи имя файла для сохранения результата в каталоге /home/mihail/Postgres- ')}"
+myfile = open(outputfile, mode='w', encoding='utf8')
+
+connection = psycopg2.connect(database="demo",
+                                user="mivainer",
+                                password="sms2851140",
+                                host="185.86.147.151",
+                                port="5432")
+
+
+cursor = connection.cursor()
+
+
+mySQLQuery = ("""
+                SELECT airport_name, city, timezone
+                FROM airports_data
+                """)
+# Получение данных по указанному запросу
+try:
+    cursor.execute(mySQLQuery)
+except Exception as err:
+    print('Ошибка при выполнении запроса к БД PostgreSQL:', err)
+
+
+results = cursor.fetchall() # только для чтения данных
+
+for num, row in enumerate(results):
+    airport_name = row[0]['ru']
+    city = row[1]['ru']
+    timezone = row[2]
+    myfile.write(f'Запись №{num}\nАэропорт - {airport_name}\nГород - {city}\nВременная зона - {timezone}\n\n')
+    print(f'Запись №{num}\nАэропорт - {airport_name}\nГород - {city}\nВременная зона - {timezone}\n\n')
+
+connection.close()
+myfile.close()
